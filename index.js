1be3aacb5c4c0c1ca9dbc701e47551804fce0530
Kou <kimura@kurashicom.jp>
2018-12-31 10:04:50 +0900

    Use vue template

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+Vue.component("global-nav", {
+  template: `
+    <div class="header-wrapper">
+      <div class="header">
+        <h2 class="header-title">Oitoku</h2>
+        <h5 class="header-subtitle">みんなの「気になる」を置いとく場所</h5>
+      </div>
+    </div>
+  `
+});
+
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     articles: null
   },
-  mounted () {
+  mounted() {
     axios
-      .get('https://script.google.com/macros/s/AKfycbzDgsTCLVWDkYatJeZmc9Y81E09_o4wC3Psg75M0tORoCfzyTE/exec')
-      .then(response => (this.articles = response.data.reverse()))
+      .get(
+        "https://script.google.com/macros/s/AKfycbzDgsTCLVWDkYatJeZmc9Y81E09_o4wC3Psg75M0tORoCfzyTE/exec"
+      )
+      .then(response => (this.articles = response.data.reverse()));
   }
-})
+});
